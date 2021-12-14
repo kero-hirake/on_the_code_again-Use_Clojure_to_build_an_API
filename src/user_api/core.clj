ebbd37d4c3b35b42450ee25abce55c5dd804b3fb
kero_hirake <k16.h.white@gmail.com>
2021-12-14 15:13:20 +0900

    ring を追加して、"hello world"を返す

--- src/user_api/core.clj
+++ src/user_api/core.clj
@@ -1,7 +1,25 @@
 (ns user-api.core
+  (:require [ring.adapter.jetty :as jetty])
   (:gen-class))
 
+(def server (atom nil))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
+(defn start-server [] 
+  (when-not @server
+    (reset! server  (jetty/run-jetty handler {:port 3000
+                                              :join? false}))))
+
+(defn stop-server []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
+
   (println "Hello, World!"))
